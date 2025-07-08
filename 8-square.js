787c9f1b5cf90de6d751e6bb233d8ae1e2364b7c
GirlCode24 <janetquayenaa@gmail.com>
2025-07-08 15:13:35 +0000

    square

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const square = Number(process.argv[2]);
+if(isNaN(square)){
+    console.log("Missing size")
+}
+else{
+    for (let i = 0; i < square; i++){
+        console.log("X".repeat(size));
+    }
+}
