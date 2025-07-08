8316c7976595ad0a216640a12d254586ef0f34a2
GirlCode24 <janetquayenaa@gmail.com>
2025-07-08 15:43:19 +0000

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1); 
+}
+
+const input = Number(process.argv[2]);
+
+if (isNaN(input)) {
+  console.log(1); 
+} else {
+  console.log(factorial(input));
+}
